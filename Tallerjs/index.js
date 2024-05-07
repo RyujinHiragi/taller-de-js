9f32f2846f764cf8c5f2d934f02db1082ed7cbd7
Ryu <123662864+RyujinHiragi@users.noreply.github.com>
2024-05-07 18:49:49 -0500

    Add files via upload

---
+++ Tallerjs/index.js
@@ -0,0 +1,31 @@
+function changeFontSize() {
+    document.getElementById("target").style.fontSize = "24px";
+}
+
+function changeTextColor() {
+    document.getElementById("target").style.color = "blue";
+}
+
+function changeWidth() {
+    document.getElementById("target").style.width = "200px";
+}
+
+function changeHeight() {
+    document.getElementById("target").style.height = "200px";
+}
+
+function changeBorderRadius() {
+    document.getElementById("target").style.borderRadius = "10px";
+}
+
+function changeMargin() {
+    document.getElementById("target").style.margin = "20px";
+}
+
+function changeBorder() {
+    document.getElementById("target").style.border = "2px dashed red";
+}
+
+function changeTextAlign() {
+    document.getElementById("target").style.textAlign = "center";
+}
